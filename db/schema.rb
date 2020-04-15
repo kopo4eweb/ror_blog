73fc0c907f6f6aa9c36cc03cc5ec752af1071777
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-15 21:41:13 +0300

    add model Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_153548) do
+ActiveRecord::Schema.define(version: 2020_04_15_183937) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.string "target"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "img"
+    t.string "title"
+    t.text "preview"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
