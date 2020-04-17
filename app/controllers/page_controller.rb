83bcd4f10ab1b4dc6f8300b6dab42835440b1d73
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-18 00:06:06 +0300

    add static page: About, Contact

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,12 @@
 class PageController < ApplicationController
     def index
     end
+
+    def about
+        @hideSubscribeInSidebar = true
+        end
+
+    def contact
+        @hideSubscribeInSidebar = true
+    end
 end
