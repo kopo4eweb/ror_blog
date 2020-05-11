02a3f00c0747eef9ee129b0c3c49caab2d9b1a9a
kopo4eweb <kopo4e.web@yandex.ru>
2020-05-11 23:42:31 +0300

    add new / edit post form, page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,50 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.paginate(page: params[:page], per_page: 1)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, success: 'Статья успешно добавлена'
+    else
+      render :new, danger: 'Статья не создана'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, success: 'Статья успешно обновлена'
+    else
+      render :edit, danger: 'Статья не обновлена'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, success: 'Статья успешно удалена'
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :preview, :content, :img)
   end
 end
