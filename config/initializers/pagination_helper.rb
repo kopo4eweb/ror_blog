53ff71d4fbdea11ff32152e12bf685cf52826787
kopo4eweb <kopo4e.web@yandex.ru>
2020-05-11 13:39:12 +0300

    customization will_paginate to posts

---
+++ config/initializers/pagination_helper.rb
@@ -0,0 +1,56 @@
+module PaginationHelper
+  class LinkRenderer < WillPaginate::ActionView::LinkRenderer
+    protected
+
+    def page_number(page)
+      if page == current_page
+        tag(:li, tag(:span, page), :class => 'active')
+      else
+        link(page, page, :rel => rel_value(page))
+      end
+    end
+
+    def gap
+      text = @template.will_paginate_translate(:page_gap) { '&hellip;' }
+      %(<li class="disabled"><a>#{text}</a></li>)
+    end
+
+    def next_page
+      num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
+      previous_or_next_page(num, @options[:next_label], 'next')
+    end
+
+    def previous_page
+      num = (@collection.current_page - 1 > 0) && @collection.current_page - 1
+      previous_or_next_page(num, @options[:previous_label], 'previous')
+    end
+
+    def previous_or_next_page(page, text, classname)
+      if page
+        link(text, page, :class => classname)
+      else
+        link(text, "#", :class => classname + ' disabled')
+      end
+    end
+
+    def html_container(html)
+      tag(:nav, tag(:ul, html ), container_attributes)
+    end
+
+    private
+
+    def link(text, target, attributes = {})
+      if target.is_a? Fixnum
+        attributes[:rel] = rel_value(target)
+        target = url(target)
+      end
+
+      unless target == "#"
+        attributes[:href] = target
+      end
+
+      attributes[:class] ||= ''
+      tag(:li, tag(:a, text, attributes))
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about' =>'page#about'
   get 'contact' => 'page#contact'
 
-  resources :posts, only: [:show]
+  resources :posts
 
   root :to => 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
